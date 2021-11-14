9e519e32711b1d13e5bc6b883d4c678cc62fa907
Oltho <17247405+Oltho@users.noreply.github.com>
2021-11-14 00:11:29 -0600

    switch to Testcase and fixture

--- tests/use_cases/test_create_todo.py
+++ tests/use_cases/test_create_todo.py
@@ -1,3 +1,7 @@
+from unittest import TestCase
+
+import pytest
+
 from todo_sample.entities.todo import Todo
 from todo_sample.repository.exception import AlreadyExistError
 from todo_sample.use_cases.create_todo import CreateTodo
@@ -6,32 +10,32 @@ from todo_sample.use_cases.exceptions import CreateTodoAlreadyExistException
 MODULE = "todo_sample.use_cases.create_todo"
 
 
-def test_create_todo(mocker):
-    fake_todo = Todo(title="title", description="description")
-    mocker.patch(f"{MODULE}.Todo", mocker.Mock(return_value=fake_todo))
+class TestCreateTodo(TestCase):
+    @pytest.fixture(autouse=True)
+    def _fake_todo_fixture(self, mocker):
+        self.fake_todo = Todo(title="title", description="description")
+        mocker.patch(f"{MODULE}.Todo", mocker.Mock(return_value=self.fake_todo))
 
-    fake_todo_repository = mocker.Mock()
+    @pytest.fixture(autouse=True)
+    def _fake_todo_repo_fixture(self, mocker):
+        self.fake_todo_repository = mocker.Mock()
 
-    create_todo_uc = CreateTodo(todo_repo=fake_todo_repository)
+    def test_create_todo(self):
+        create_todo_uc = CreateTodo(todo_repo=self.fake_todo_repository)
 
-    created_todo = create_todo_uc.call(title="title", description="description")
+        created_todo = create_todo_uc.call(title="title", description="description")
 
-    fake_todo_repository.save.assert_called_once_with(todo=fake_todo, create_only=True)
-    assert created_todo == fake_todo
+        self.fake_todo_repository.save.assert_called_once_with(todo=self.fake_todo, create_only=True)
+        assert created_todo == self.fake_todo
 
+    def test_create_todo_already_exist(self):
+        self.fake_todo_repository.save.side_effect = AlreadyExistError()
 
-def test_create_todo_already_exist(mocker):
-    fake_todo = Todo(title="title", description="description")
-    mocker.patch(f"{MODULE}.Todo", mocker.Mock(return_value=fake_todo))
+        create_todo_uc = CreateTodo(todo_repo=self.fake_todo_repository)
 
-    fake_todo_repository = mocker.Mock()
-    fake_todo_repository.save.side_effect = AlreadyExistError()
+        try:
+            create_todo_uc.call(title="title", description="description")
+        except CreateTodoAlreadyExistException as exc:
+            assert str(self.fake_todo.id) in str(exc)
 
-    create_todo_uc = CreateTodo(todo_repo=fake_todo_repository)
-
-    try:
-        create_todo_uc.call(title="title", description="description")
-    except CreateTodoAlreadyExistException as exc:
-        assert str(fake_todo.id) in str(exc)
-
-    fake_todo_repository.save.assert_called_once_with(todo=fake_todo, create_only=True)
+        self.fake_todo_repository.save.assert_called_once_with(todo=self.fake_todo, create_only=True)
