2fe474d44af32b41798a3d2fae2c5f7cb3a411e0
Oltho <17247405+Oltho@users.noreply.github.com>
2021-11-14 01:03:08 -0600

    update exceptions definition with pass (valid lint)

--- src/todo_sample/use_cases/exceptions.py
+++ src/todo_sample/use_cases/exceptions.py
@@ -1,18 +1,28 @@
 class CreateTodoException(Exception):
     """CreateTodoException"""
 
+    pass
+
 
 class CreateTodoAlreadyExistException(CreateTodoException):
     """CreateTodoAlreadyExistException"""
+
+    pass
 
 
 class GetTodoException(Exception):
     """GetTodoException"""
 
+    pass
+
 
 class GetTodoInvalidIdFormatException(GetTodoException):
     """GetTodoInvalidIdFormatException"""
 
+    pass
+
 
 class GetTodoNotFoundException(GetTodoException):
     """GetTodoNotFoundException"""
+
+    pass
