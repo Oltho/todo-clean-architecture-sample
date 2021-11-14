a4096992c0504ab3aac2df94bc15990cfb2acdc5
Oltho <17247405+Oltho@users.noreply.github.com>
2021-11-14 01:00:35 -0600

    reformat test

--- tests/use_cases/test_create_todo.py
+++ tests/use_cases/test_create_todo.py
@@ -19,22 +19,19 @@ class TestCreateTodo(TestCase):
     @pytest.fixture(autouse=True)
     def _fake_todo_repo_fixture(self, mocker):
         self.fake_todo_repository = mocker.Mock()
+        self.create_todo_uc = CreateTodo(todo_repo=self.fake_todo_repository)
 
     def test_create_todo(self):
-        create_todo_uc = CreateTodo(todo_repo=self.fake_todo_repository)
-
-        created_todo = create_todo_uc.call(title="title", description="description")
+        created_todo = self.create_todo_uc.call(title="title", description="description")
 
         self.fake_todo_repository.save.assert_called_once_with(todo=self.fake_todo, create_only=True)
-        assert created_todo == self.fake_todo
+        assert self.fake_todo == created_todo
 
     def test_create_todo_already_exist(self):
         self.fake_todo_repository.save.side_effect = AlreadyExistError()
 
-        create_todo_uc = CreateTodo(todo_repo=self.fake_todo_repository)
-
         try:
-            create_todo_uc.call(title="title", description="description")
+            self.create_todo_uc.call(title="title", description="description")
         except CreateTodoAlreadyExistException as exc:
             assert str(self.fake_todo.id) in str(exc)
 
