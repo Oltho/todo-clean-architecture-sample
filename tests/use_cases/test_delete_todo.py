f0e2d76460a70de694d135d39d5f1476720ec6c6
Oltho <17247405+Oltho@users.noreply.github.com>
2021-11-14 22:28:40 -0600

    add comment to duplicated testing

--- tests/use_cases/test_delete_todo.py
+++ tests/use_cases/test_delete_todo.py
@@ -20,6 +20,7 @@ class TestGetTodo(TestCase):
         self.delete_todo_uc = DeleteTodo(todo_repo=self.fake_todo_repository)
 
     def test_delete_todo(self):
+        """we could mock GetTodo, but lets try to avoid to much mock (1662232f61aa9a1fe449f66fec97bf63042e5a47)"""
         self.fake_todo_repository.find_by_id.return_value = self.fake_todo
 
         ret = self.delete_todo_uc.call(id=str(self.fake_todo.id))
@@ -29,6 +30,7 @@ class TestGetTodo(TestCase):
         assert ret is None
 
     def test_delete_todo_wrong_id_format(self):
+        """Keeping the test but this is already tested in test_get_todo_wrong_id_format"""
         fake_id = "some-wrong-format-id"
         try:
             self.delete_todo_uc.call(id=fake_id)
@@ -36,6 +38,7 @@ class TestGetTodo(TestCase):
             assert fake_id in str(exc)
 
     def test_get_todo_not_found(self):
+        """Keeping the test but this is already tested in test_get_todo_not_found"""
         self.fake_todo_repository.find_by_id.return_value = None
 
         try:
