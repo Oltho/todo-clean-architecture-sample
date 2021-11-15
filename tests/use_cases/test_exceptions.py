ec092cfef4adf8ed9885dee9d4d2fc32ad67e103
Oltho <17247405+Oltho@users.noreply.github.com>
2021-11-14 22:06:09 -0600

    test exception str

--- tests/use_cases/test_exceptions.py
+++ tests/use_cases/test_exceptions.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from todo_sample.use_cases.exceptions import TodoNotFoundException
+from todo_sample.use_cases.exceptions import TodoAlreadyExistException, TodoNotFoundException
 
 
 def test_todonotfoundexception():
@@ -9,3 +9,11 @@ def test_todonotfoundexception():
 
     assert exc.id == fake_id
     assert f"Todo with id:{fake_id} not found." == str(exc)
+
+
+def test_todoalreadyexistexception():
+    fake_id = uuid4()
+    exc = TodoAlreadyExistException(id=fake_id)
+
+    assert exc.id == fake_id
+    assert f"Todo with id:{fake_id} already exist." == str(exc)
